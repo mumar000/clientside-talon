284f16781b5a2cec525eb11ab876f48565b0dd0c
Muhammad Umar <umarabdulhaque@gmail.com>
2025-05-28 11:31:04 +0500

    feat:add auto logout when session expired

---
+++ src/api/axiosClient.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+import { logout } from "../store/features/authSlice";
+import store from "../store/store";
+
+const baseURL = import.meta.env.DEV
+  ? import.meta.env.VITE_LOCAL_URL
+  : import.meta.env.VITE_DEPLOYED_URL;
+
+const axiosClient = axios.create({
+  baseURL,
+  withCredentials: true,
+});
+
+axiosClient.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      store.dispatch(logout());
+      window.location.href = "/login";
+    }
+    return Promise.reject(error);
+  }
+);
